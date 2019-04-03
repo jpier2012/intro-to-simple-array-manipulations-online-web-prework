f8aeda283c4c3a889f0c648a618a480cc6a7709b
James Pier <jpier2012@gmail.com>
2019-04-03 12:27:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,10 @@ def using_push array string
   array.push string
 end
 
+puts using_push(["1","2","3"],"4")
+
 def using_unshift array string
   array.unshift string
 end
+
+puts using_unshift(["1","2","3"],"4")
